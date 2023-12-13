ea73f5931273b04bcff8ba50505730acf0bb4e3e
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2023-12-12 22:31:21 -0300

    Fix

--- mycows/src/views/Login.vue
+++ mycows/src/views/Login.vue
@@ -1,38 +1,38 @@
 <template>
-    <v-container fluid class="py-0 px-0" fill-height>
-        <v-card color="info" class="custom-card">
+    <div class="d-flex align-center justify-center" style="height: 100vh;  background-color: #2196F3;">
+        <v-sheet rounded width="500" height="450" class="mx-auto" color="white">
             <v-form v-model="valid">
-                <v-card class="mx-auto mt-5" max-width="400" color="white">
-                    <h2 class="text-center mt-6">
-                        Cadastro
-                    </h2>
-                    <v-row justify="center">
-                        <v-col cols="12" class="mt-6" md="8">
-                            <v-text-field v-model="username" :rules="nameRules" :counter="10" label="Usuário" required
-                                hide-details></v-text-field>
-                        </v-col>
-                    </v-row>
-                    <v-row justify="center">
-                        <v-col cols="12" md="8">
-                            <v-text-field v-model="password" :type="show1 ? 'text' : 'password'" name="input-10-1"
-                                label="Senha" hint="Pelo menos 8 caracteres" @click:append="show1 = !show1">
-                            </v-text-field>
-                        </v-col>
-                    </v-row>
-                    <v-row>
-                        <v-col cols="6" class="ml-6">
-                            <v-checkbox label="Permanecer conectado"></v-checkbox>
-                        </v-col>
-                        <v-col cols="2" class="ml-2">
-                            <v-btn color="info">
-                                Login
-                            </v-btn>
-                        </v-col>
-                    </v-row>
-                </v-card>
+                <h2 class="text-center mt-6">
+                    Cadastro
+                </h2>
+                <v-row justify="center">
+                    <v-col cols="9">
+                        <v-text-field v-model="username" :rules="nameRules" :counter="10" label="Usuário" required
+                            hide-details>
+                        </v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row justify="center">
+                    <v-col cols="9">
+                        <v-text-field v-model="password" :type="show1 ? 'text' : 'password'" name="input-10-1" label="Senha"
+                            hint="Pelo menos 8 caracteres" @click:append="show1 = !show1">
+                        </v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row justify="center">
+                    <v-col cols="9">
+                        <a href="#" class="text-body-2 font-weight-regular">Esqueceu a senha?</a>
+                    </v-col>
+                    <v-col cols="9">
+                        <v-btn type="submit" color="info" block>Login</v-btn>
+                    </v-col>
+                    <v-col cols="9">
+                        <a href="#" class="text-body-2 font-weight-regular">Não tem conta? Cadastre-se</a>
+                    </v-col>
+                </v-row>
             </v-form>
-        </v-card>
-    </v-container>
+        </v-sheet>
+    </div>
 </template>
 
 <script>
