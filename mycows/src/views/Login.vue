9dc0b7337b865c8f7b6535565713397bd529e051
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2023-12-15 02:17:30 -0300

    Add logos

--- mycows/src/views/Login.vue
+++ mycows/src/views/Login.vue
@@ -2,9 +2,9 @@
     <div class="d-flex align-center justify-center" style="height: 100vh;  background-color: #0277BD;">
         <v-sheet rounded width="500" height="450" class="mx-auto" color="white" elevation=16>
             <v-form>
-                <h2 class="text-center mt-6">
-                    Login
-                </h2>
+                <v-row justify="center" class="mt-8 mb-6">
+                    <img :src="logoImg" alt="">
+                </v-row>
                 <v-row justify="center" class="mt-6">
                     <v-col cols="9">
                         <v-text-field prepend-inner-icon="mdi-email" variant="solo" rounded v-model="username" :counter="10" label="E-mail" required
@@ -26,10 +26,10 @@
                 </v-row>
                 <v-row justify="center">
                     <v-col cols="4">
-                        <a href="#" class="text-body-2 font-weight-regular">Esqueceu a senha?</a>
+                        <a href="#" class="custom-link text-body-2 font-weight-regular">Esqueceu a senha?</a>
                     </v-col>
                     <v-col cols="5">
-                        <a href="#" class="text-body-2 font-weight-regular">Não tem conta? Cadastre-se</a>
+                        <a href="#" class="custom-link text-body-2 font-weight-regular">Não tem conta? Cadastre-se</a>
                     </v-col>
                 </v-row>
             </v-form>
@@ -48,6 +48,8 @@
 <script>
 
 import {login} from '../services/authentication.js'
+import logoImg from '../components/icons/logomeugado.png'
+import logoImg2 from '../components/icons/logomeugado2.png'
 
 export default {
     data() {
@@ -57,7 +59,8 @@ export default {
             show1: false,
             loginError: false,
             errorMsg: '',
-            loginErrorColor: 'red'
+            loginErrorColor: 'red',
+            logoImg: logoImg
         };
     },
     methods: {
@@ -85,5 +88,9 @@ export default {
 <style scoped>
 .custom-card {
     border-radius: 0;
+}
+.custom-link{
+    color: #0277BD;
+    text-decoration: none;
 }
 </style>
