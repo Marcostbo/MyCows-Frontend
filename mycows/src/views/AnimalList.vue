54d949a6cb9c6fcc461f4cd4587f4fe3261031dd
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2023-12-14 22:18:35 -0300

    Add age

--- mycows/src/views/AnimalList.vue
+++ mycows/src/views/AnimalList.vue
@@ -3,25 +3,16 @@
     <v-container>
         <v-row>
             <v-col cols="8" offset="2">
-                <v-card
-                    class="mb-2"
-                    width="400"
-                    title="Lista de Animais"
-                    subtitle="Pàgina para listar, buscar e editar o gado"
-                    style="border-left: 4px solid #0277BD;"
+                <v-card class="mb-2" width="400" title="Lista de Animais"
+                    subtitle="Pàgina para listar, buscar e editar o gado" style="border-left: 4px solid #0277BD;"
                     elevation=0>
                 </v-card>
             </v-col>
         </v-row>
         <v-row>
             <v-col cols="4" offset="2">
-                <v-text-field
-                variant="solo"
-                v-model="animalName" 
-                label="Nome"
-                filled 
-                rounded
-                append-inner-icon="mdi-magnify">
+                <v-text-field variant="solo" v-model="animalName" label="Nome" filled rounded
+                    append-inner-icon="mdi-magnify">
                 </v-text-field>
             </v-col>
             <v-col cols="2">
@@ -44,9 +35,9 @@
             </v-col>
         </v-row>
         <v-row>
-            <v-col cols="8" offset="2">
+            <v-col cols="2" offset="9">
                 <v-btn density="comfortable" outlined color="light-blue-darken-3" @click="resetAnimalList">
-                    Resetar
+                    Reset
                 </v-btn>
             </v-col>
         </v-row>
@@ -54,7 +45,7 @@
 </template>
 
 <script>
-
+// https://vuetifyjs.com/en/components/data-tables/basics/#crud-actions look here later
 import SideBar from '@/components/Sidebar.vue';
 import { listAnimals } from '../services/animal.js'
 
@@ -65,13 +56,14 @@ export default {
             animalShow: [],
             animalName: '',
             tableHeaders: [
-                {title: 'Nome', value: 'name', sortable: false, align: 'center'},
-                {title: 'Nascimento', value: 'birth_date', sortable: false, align: 'center'},
-                {title: 'Sexo', value: 'animal_sex', sortable: false, align: 'center'},
-                {title: 'Tipo', value: 'animal_type', sortable: false, align: 'center'},
-                {title: 'Origem', value: 'origin', sortable: false, align: 'center'},
-                {title: 'Detalhes', value: 'detail', sortable: false, align: 'center'},
-                {title: 'Editar', value: 'edit', sortable: false, align: 'center'},
+                { title: 'Nome', value: 'name', sortable: false, align: 'center' },
+                { title: 'Nascimento', value: 'birth_date', sortable: false, align: 'center' },
+                { title: 'Idade', value: 'age', sortable: false, align: 'center' },
+                { title: 'Sexo', value: 'animal_sex', sortable: false, align: 'center' },
+                { title: 'Tipo', value: 'animal_type', sortable: false, align: 'center' },
+                { title: 'Origem', value: 'origin', sortable: false, align: 'center' },
+                { title: 'Detalhes', value: 'detail', sortable: false, align: 'center' },
+                { title: 'Editar', value: 'edit', sortable: false, align: 'center' },
             ]
         }
     },
@@ -88,7 +80,6 @@ export default {
             console.log('Editing animal:', item);
         },
         searchAnimal() {
-            console.log(this.animalList);
             this.animalShow = this.animalList.filter(animal => animal.name === this.animalName);
         },
         resetAnimalList() {
