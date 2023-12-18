6b8f21b8c04d0f00981574d6747e02d117a5af8c
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2023-12-18 18:53:06 -0300

    start edit dialog development

---
+++ mycows/src/components/EditAnimalDialog.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-dialog v-model="showDialog" width="auto">
+        <v-card>
+            <v-card-text>
+                {{ animal }}
+            </v-card-text>
+            <v-card-actions>
+                <v-btn color="primary" block @click="closeDialog">
+                    Close Dialog
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+
+export default {
+    props: {
+        animal: {},
+        editAnimal: false,
+        closeHandler: Function,
+    },
+    data() {
+        return {
+            showDialog: false
+        }
+    },
+    watch: {
+        editAnimal(newVal){
+            this.showDialog = newVal;
+            // console.log(newVal);
+            // if (newVal){
+            //     this.showDialog = true;
+            // }
+        }
+    },
+    methods: {
+        closeDialog() {
+            this.showDialog = false;
+            this.closeHandler();
+        }
+    } 
+}
+
+</script>
--- mycows/src/views/AnimalList.vue
+++ mycows/src/views/AnimalList.vue
@@ -25,11 +25,11 @@
             <v-col cols="8" offset="2">
                 <v-data-table :items="animalShow" :headers="tableHeaders" elevation=4>
                     <template v-slot:item.detail="{ item }">
-                        <v-icon @click="editAnimal(item)">mdi-plus-box-multiple-outline</v-icon>
+                        <v-icon @click="showDetailAnimalDialog(item)">mdi-plus-box-multiple-outline</v-icon>
                     </template>
                     <template v-slot:item.edit="{ item }">
-                        <v-icon @click="editAnimal(item)">mdi-pencil</v-icon>
-                        <v-icon @click="editAnimal(item)">mdi-delete</v-icon>
+                        <v-icon @click="showEditAnimalDialog(item)">mdi-pencil</v-icon>
+                        <v-icon @click="showDetailAnimalDialog(item)">mdi-delete</v-icon>
                     </template>
                 </v-data-table>
             </v-col>
@@ -42,11 +42,16 @@
             </v-col>
         </v-row>
     </v-container>
+    <edit-animal-dialog
+        :animal="animalDialog"
+        :editAnimal="editAnimal" 
+        :closeHandler="closeEditAnimalDialog">
+    </edit-animal-dialog>
 </template>
 
 <script>
-// https://vuetifyjs.com/en/components/data-tables/basics/#crud-actions look here later
 import SideBar from '@/components/Sidebar.vue';
+import EditAnimalDialog from '@/components/EditAnimalDialog.vue';
 import { listAnimals } from '../services/animal.js'
 
 export default {
@@ -55,6 +60,7 @@ export default {
             animalList: [],
             animalShow: [],
             animalName: '',
+            animalDialog: {},
             tableHeaders: [
                 { title: 'Nome', value: 'name', sortable: false, align: 'center' },
                 { title: 'Nascimento', value: 'birth_date', sortable: false, align: 'center' },
@@ -64,11 +70,15 @@ export default {
                 { title: 'Origem', value: 'origin', sortable: false, align: 'center' },
                 { title: 'Detalhes', value: 'detail', sortable: false, align: 'center' },
                 { title: 'Editar', value: 'edit', sortable: false, align: 'center' },
-            ]
+            ],
+            editAnimal: false,
+            deleteAnimal: false,
+            detailtAnimal: false
         }
     },
     components: {
         SideBar,
+        EditAnimalDialog
     },
     created: async function () {
         const token = localStorage.getItem('Token');
@@ -76,9 +86,29 @@ export default {
         this.animalShow = this.animalList;
     },
     methods: {
-        editAnimal(item) {
-            console.log('Editing animal:', item);
+        // Edit Animal handlers
+        showEditAnimalDialog(item) {
+            this.animalDialog = item;
+            this.editAnimal = true;
         },
+        closeEditAnimalDialog() {
+            this.editAnimal = false;
+        },
+        // Detail Animal handlers
+        showDetailAnimalDialog(item) {
+            this.detailtAnimal = true;
+        },
+        closeDetailAnimalDialog(item) {
+            this.detailtAnimal = false;
+        },
+        // Delete Animal handlers
+        showDeleteAnimalDialog(item) {
+            this.deleteAnimal = true;
+        },
+        closeDeleteAnimalDialog(item) {
+            this.deleteAnimal = false;
+        },
+        // Animal query
         searchAnimal() {
             this.animalShow = this.animalList.filter(animal => animal.name === this.animalName);
         },
