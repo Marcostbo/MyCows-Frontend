fb33e262321d6f132ef97c631f447866ceaa83d9
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2023-12-18 19:20:37 -0300

    create update pt2

--- mycows/src/components/EditAnimalDialog.vue
+++ mycows/src/components/EditAnimalDialog.vue
@@ -1,13 +1,67 @@
 <template>
     <v-dialog v-model="showDialog" width="auto">
-        <v-card>
+        <v-card width="600" height="350">
+            <v-card-title>
+                Editar {{ animal.name }}
+            </v-card-title>
             <v-card-text>
-                {{ animal }}
+                <v-container>
+                    <v-row>
+                        <v-col>
+                            <v-text-field
+                                v-model="animal.name"
+                                label="Nome"
+                            >
+                            </v-text-field>
+                        </v-col>
+                        <v-col>
+                            <v-text-field
+                                v-model="animal.birth_date"
+                                label="Nascimento"
+                                >
+                            </v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col>
+                            <v-text-field
+                                v-model="animal.origin"
+                                label="Origem"
+                                >
+                            </v-text-field>
+                        </v-col>
+                        <v-col>
+                            <v-select
+                                v-model="animal.animal_sex"
+                                :items="['Macho', 'Fêmea']"
+                                label="Gênero"
+                            >
+                            </v-select>
+                        </v-col>
+                        <v-col>
+                            <v-select
+                                v-model="animal.animal_type"
+                                :items="['Boi', 'Vaca', 'Bezerro', 'Novilha']"
+                                label="Tipo"
+                            >
+                            </v-select>
+                        </v-col>
+                    </v-row>
+                </v-container>
             </v-card-text>
             <v-card-actions>
-                <v-btn color="primary" block @click="closeDialog">
-                    Close Dialog
-                </v-btn>
+                <v-row>
+                    <v-col>
+                        <v-btn color="red" block @click="closeDialog">
+                            Fechar
+                        </v-btn>
+                    </v-col>
+                    <v-col>
+                        <v-btn color="success" block @click="saveChanges">
+                            Salvar
+                        </v-btn>
+                    </v-col>
+                </v-row>
             </v-card-actions>
         </v-card>
     </v-dialog>
@@ -29,16 +83,15 @@ export default {
     watch: {
         editAnimal(newVal){
             this.showDialog = newVal;
-            // console.log(newVal);
-            // if (newVal){
-            //     this.showDialog = true;
-            // }
         }
     },
     methods: {
         closeDialog() {
             this.showDialog = false;
             this.closeHandler();
+        },
+        saveChanges() {
+            this.closeDialog();
         }
     } 
 }
